cc53bc3f585c1c84f4973ac67122160b9b0fe1f8
daiya-610 <daiya06010@gmail.com>
2025-10-30 19:27:50 +0900

    fix: ListTemplate.js追加、childrenに対してパラメーターを引き渡す

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-/** TitledPanel.js */
+/** ListTemplate.js */
+import books from "./03/books.json";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import TitledPanel from "./02/TitledPanel";
+import ListTemplate from "./03/ListTemplate";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <TitledPanel>
-    <p key="title">2025/10/29</p>
-    <p key="body">勉強スタート</p>
-  </TitledPanel>
+  <ListTemplate src={books}>
+    {(elem) => {
+      <>
+        <dt>
+          <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+            {elem.title} ({elem.price}円)
+          </a>
+        </dt>
+        <dd>{elem.summary}</dd>
+      </>;
+    }}
+  </ListTemplate>
 );
+/** ListTemplate.js */
 
+/** TitledPanel.js */
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import TitledPanel from "./02/TitledPanel";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <TitledPanel>
+//     <p key="title">2025/10/29</p>
+//     <p key="body">勉強スタート</p>
+//   </TitledPanel>
+// );
 /** TitledPanel.js */
 
 /** StyledPanel.js */
