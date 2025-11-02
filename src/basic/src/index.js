7e82c161f1cdd48cd79219b5cbe90b2b3d48081b
daiya-610 <daiya06010@gmail.com>
2025-11-02 18:47:11 +0900

    add: StateFormUC.js追加　非制御コンポーネントによるフォーム管理

--- src/basic/src/index.js
+++ src/basic/src/index.js
@@ -1,5 +1,15 @@
-import StateForm from "./01/StateForm";
+/** StateFormUC.js */
+import StateFormUC from "./02/StateFormUC";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<StateForm />);
+root.render(<StateFormUC />);
+/** StateFormUC.js */
+
+/** StateForm.js */
+// import StateForm from "./01/StateForm";
+// import ReactDOM from "react-dom/client";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<StateForm />);
+/** StateForm.js */
