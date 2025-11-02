2b2713f964f5a0ac4ffb651f9752160e9f7576c5
daiya-610 <daiya06010@gmail.com>
2025-11-02 17:05:26 +0900

    fix: イベント規定の動作をキャンセルする(preventDefault)

--- src/14/EventPropagation.js
+++ src/14/EventPropagation.js
@@ -3,7 +3,7 @@ export default function EventPropagation() {
   const handleParent = () => alert("#parent run...");
   const handleMy = () => alert("#my run...");
   const handleChild = (e) => {
-    e.stopPropagation();
+    e.preventDefault();
     alert("#child run...");
   };
 
