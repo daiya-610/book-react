0adade30102b4b8b411af86d16d4c188986d50b6
daiya-610 <daiya06010@gmail.com>
2025-11-03 10:16:18 +0900

    add: FormSelect.js追加、選択ボックス

--- src/basic/src/index.js
+++ src/basic/src/index.js
@@ -1,9 +1,17 @@
-/** FormTextarea.js */
-import FormTextarea from "./03/FormTextarea";
+/** FormSelect.js */
+import FormSelect from "./04/FormSelect";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FormTextarea />);
+root.render(<FormSelect />);
+/** FormSelect.js */
+
+/** FormTextarea.js */
+// import FormTextarea from "./03/FormTextarea";
+// import ReactDOM from "react-dom/client";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<FormTextarea />);
 /** FormTextarea.js */
 
 /** StateFormUC.js */
