7e9b10fc2176c0456382922cb02bdaa5de9e15b4
daiya-610 <daiya06010@gmail.com>
2025-11-03 14:25:01 +0900

    remove: 余分なコメント削除

--- src/basic/src/11/todo-done/StateTodo.js
+++ src/basic/src/11/todo-done/StateTodo.js
@@ -1,12 +1,3 @@
-// 配列型のStateに関しても、オブジェクト型と同じ理由で直接更新することができない。
-/**
- * 操作            | 利用すべき（不変更新）             | 避けるべき（ミューテート）
- * 追加            | concat, [...list, newItem]          | push, unshift
- * 削除            | filter, [...list.slice(0, i), ...list.slice(i+1)] | splice, list[i] = …
- * 変換（変形）     | map                                   | for-ループ 内で list[i] を直接変更
- * 並び替え（ソート）| [...list].sort(), [...list].reverse() | sort(), reverse()
- */
-
 import { useState } from "react";
 import "./StateTodo.css";
 
